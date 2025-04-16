1d5706bed06dddba42bac83e12584dd4cb053f1b
Mukesh reddy <mukheshreddycheerla@gmail.com>
2025-04-16 18:50:25 +0530

    Update app.py

--- hello/app.py
+++ hello/app.py
@@ -337,5 +337,5 @@ def get_donors():
     conn.close()
     return donors
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
