8f993a443c8325fe165d193ef0432376686cb2ce
Mukesh reddy <mukheshreddycheerla@gmail.com>
2025-04-16 22:43:12 +0530

    Create base.js

---
+++ hello/static/base.js
@@ -0,0 +1,6 @@
+                    setTimeout(function() {
+                        const alertContainer = document.getElementById('alert-container');
+                        if (alertContainer) {
+                            alertContainer.remove();
+                        }
+                    }, 3000); // Remove alerts after 3 seconds (adjust as needed)
