9ff34bb7512f874a3b98209da2a41125e703c89c
Mukesh reddy <mukheshreddycheerla@gmail.com>
2025-04-18 18:14:44 +0530

    Update app.py

--- hello/app.py
+++ hello/app.py
@@ -15,7 +15,7 @@ app.secret_key = os.urandom(24)
 db_config = {}
 
 # MySQL Configuration
-DATABASE_URL = os.environ.get('MYSQL_URL')
+DATABASE_URL = os.environ.get('PUBLIC_MYSQL_URL')
 print(f"DATABASE_URL: {DATABASE_URL}")  # Add this line
 
 if DATABASE_URL:
